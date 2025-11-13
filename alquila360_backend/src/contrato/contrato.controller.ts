e72390c41fab520840d1ccd7e6e2c47fb8638045
J-Flores <cristiangfc18@gmail.com>
2025-11-13 14:11:32 -0400

    .

---
+++ alquila360_backend/src/contrato/contrato.controller.ts
@@ -0,0 +1,36 @@
+// src/contrato/contrato.controller.ts
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ContratoService } from './contrato.service';
+import { CreateContratoDto } from './dto/create-contrato.dto';
+import { UpdateContratoDto } from './dto/update-contrato.dto';
+
+@Controller('contrato') // Endpoint base: /contrato
+export class ContratoController {
+    constructor(private readonly contratoService: ContratoService) {}
+
+    @Post()
+    create(@Body() createContratoDto: CreateContratoDto) {
+        return this.contratoService.create(createContratoDto);
+    }
+
+    @Get()
+    findAll() {
+        return this.contratoService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        // Se usa +id para convertir el string del parámetro a número
+        return this.contratoService.findOne(+id);
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateContratoDto: UpdateContratoDto) {
+        return this.contratoService.update(+id, updateContratoDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.contratoService.remove(+id);
+    }
+}
