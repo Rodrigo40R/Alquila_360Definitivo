bce569bf2caa14fc22f3d8d14fef92968f7ec779
root <adrianmercado782@gmail.com>
2025-11-23 19:27:39 -0400

    back con front conectado

--- alquila360_backend/src/auth/auth.module.ts
+++ alquila360_backend/src/auth/auth.module.ts
@@ -9,7 +9,7 @@ import { JwtStrategy } from '././jwt.strategy';
   imports: [
     UserModule,
     JwtModule.register({
-        secret: process.env.JWT_SECRET,
+        secret: process.env.JWT_SECRET || 'super_secreto_cambia_esto',
         signOptions: { expiresIn: '1h' },
     }),
 
