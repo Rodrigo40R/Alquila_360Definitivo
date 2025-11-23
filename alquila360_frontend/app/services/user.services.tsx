4bf44b63ed17a6cd72557cd8af73c64ed3f52388
root <adrianmercado782@gmail.com>
2025-11-23 19:28:26 -0400

    back con front conectado

--- alquila360_frontend/app/services/user.services.tsx
+++ alquila360_frontend/app/services/user.services.tsx
@@ -1,26 +1,37 @@
-import { instance } from "../utils/axios.util"
+import { instance } from "../utils/axios.util";
+
+export interface User {
+  id_usuario: number;
+  nombre: string;
+  correo: string;
+  tipo_usuario: string;
+  verificado: boolean;
+  estado_cuenta: string;
+}
+
+const BASE_PATH = "/users";
 
 export const getUsers = async () => {
-    const reponse = await instance.get("/user");
-    return reponse.data;
-}
+  const response = await instance.get(BASE_PATH);
+  return response.data;
+};
 
 export const createUser = async (user: Partial<User>) => {
-    const response = await instance.post("/user", user);
-    return response.data;
-}
+  const response = await instance.post(BASE_PATH, user);
+  return response.data;
+};
 
 export const getUserById = async (id: number) => {
-    const response = await instance.get(`/user/${id}`);
-    return response.data;
-}
+  const response = await instance.get(`${BASE_PATH}/${id}`);
+  return response.data;
+};
 
 export const updateUser = async (id: number, userData: Partial<User>) => {
-    const response = await instance.put(`/user/${id}`, userData);
-    return response.data;
-}
+  const response = await instance.put(`${BASE_PATH}/${id}`, userData);
+  return response.data;
+};
 
 export const deleteUser = async (id: number) => {
-    const response = await instance.delete(`/user/${id}`);
-    return response.data;
-}
+  const response = await instance.delete(`${BASE_PATH}/${id}`);
+  return response.data;
+};
