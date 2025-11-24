c07f354e90c22ad8efda0b5563e262f8a512f406
Rodrigo Villarroel <rovillaaya10@gmail.com>
2025-11-24 18:22:58 -0400

    cambios json

--- alquila360_frontend/postcss.config.mjs
+++ alquila360_frontend/postcss.config.mjs
@@ -1,7 +1,9 @@
-import tailwindcss from "@tailwindcss/postcss";
-
-export default {
+/** @type {import('postcss-load-config').Config} */
+const config = {
   plugins: {
-    [tailwindcss]: {},
+    '@tailwindcss/postcss': {},  // <--- ESTA es la línea nueva clave
+    autoprefixer: {},
   },
 };
+
+export default config;
