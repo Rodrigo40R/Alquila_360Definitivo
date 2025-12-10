c1858481d0eecd49c0bae90703fdaa7d9ae730e4
alexandra <alex@DESKTOP-NI6T640.localdomain>
2025-12-10 11:49:25 -0400

    feat(backend): agregar inyección de repositorios en UserModule
    - Importar TypeOrmModule para Contrato, Cuota y Multa
    - Registrar proveedores CONTRATO_REPOSITORY, CUOTA_REPOSITORY, MULTA_REPOSITORY
    - Permite que UserService acceda a estos repositorios

--- alquila360_backend/src/user/user.module.ts
+++ alquila360_backend/src/user/user.module.ts
@@ -6,11 +6,21 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 import { User } from '../entity/user.entity';
+import { Contrato } from '../entity/contrato.entity';
+import { Cuota } from '../entity/cuota.entity';
+import { Multa } from '../entity/multa.entity';
+
 import { USER_REPOSITORY } from './ports/user.repo';
 import { UserTypeOrmRepository } from './adapters/user.repo.typeorm';
+import { CONTRATO_REPOSITORY } from '../contrato/ports/contrato.repo';
+import { ContratoTypeOrmRepository } from '../contrato/adapters/contrato.repo.typeorm';
+import { CUOTA_REPOSITORY } from '../cuota/ports/cuota.repo';
+import { CuotaTypeOrmRepository } from '../cuota/adapters/cuota.repo.typeorm';
+import { MULTA_REPOSITORY } from '../multa/ports/multa.repo';
+import { MultaTypeOrmRepository } from '../multa/adapters/multa.repo.typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Contrato, Cuota, Multa])],
   controllers: [UserController],
   providers: [
     UserService,
@@ -18,10 +28,25 @@ import { UserTypeOrmRepository } from './adapters/user.repo.typeorm';
       provide: USER_REPOSITORY,
       useClass: UserTypeOrmRepository,
     },
+    {
+      provide: CONTRATO_REPOSITORY,
+      useClass: ContratoTypeOrmRepository,
+    },
+    {
+      provide: CUOTA_REPOSITORY,
+      useClass: CuotaTypeOrmRepository,
+    },
+    {
+      provide: MULTA_REPOSITORY,
+      useClass: MultaTypeOrmRepository,
+    },
   ],
   exports: [
     UserService,
-    USER_REPOSITORY, // 👈 esto permite que otros módulos inyecten el repo de usuario
+    USER_REPOSITORY,
+    CONTRATO_REPOSITORY,
+    CUOTA_REPOSITORY,
+    MULTA_REPOSITORY,
   ],
 })
 export class UserModule {}
